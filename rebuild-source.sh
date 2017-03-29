3983fe93175bf49d9f4ba029a63e1864e0ee0865
Ho Song Yan <songyan.ho@shopeemobile.com>
2017-03-29 17:51:05 +0800

    Bug fix

--- rebuild-source.sh
+++ rebuild-source.sh
@@ -1,3 +1,5 @@
 pip uninstall -y superset
 cd /home/songyan.ho/shopee-superset/superset/assets && npm install && npm run build
-cd /home/songyan.ho/shopee-superset && python setup.py install)
+cd /home/songyan.ho/shopee-superset && python setup.py install
+
+superset runserver
