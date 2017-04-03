b812c863fd8cf38249d05b7bfca9df1b569fe963
Ho Song Yan <songyan.ho@shopeemobile.com>
2017-04-03 17:32:17 +0800

    Separate worker init script

--- run_production.sh
+++ run_production.sh
@@ -1,6 +1,3 @@
 #!/usr/bin/env bash
 
-export C_FORCE_ROOT=True
-su - songyan.ho -c "superset worker" &
-superset runserver &
-wait
+superset runserver
---
+++ run_worker.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+export C_FORCE_ROOT=True
+superset worker
