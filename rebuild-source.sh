268ee7af37bfdfd45c3cfdae46debab855f7f293
Ho Song Yan <songyan.ho@shopeemobile.com>
2017-04-03 17:36:17 +0800

    Added services enabler and terminator

--- rebuild-source.sh
+++ rebuild-source.sh
@@ -2,9 +2,12 @@
 
 pip uninstall -y superset
 systemctl stop superset
+systemctl stop superset-worker
 cd /home/songyan.ho/shopee-superset && git checkout . && git pull origin master
 cd /home/songyan.ho/shopee-superset/superset/assets && npm install && npm run build
 cd /home/songyan.ho/shopee-superset && python setup.py install
 cd /home/songyan.ho/shopee-superset && chmod +x ./rebuild-source.sh
 systemctl enable superset
 systemctl start superset
+systemctl enable superset-worker
+systemctl start superset-worker
