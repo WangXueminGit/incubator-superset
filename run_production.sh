7133fb373ff3ec658cea290e020a47e741a3aca5
Ho Song Yan <songyan.ho@shopeemobile.com>
2017-04-04 17:37:50 +0800

    Bash script updated

--- run_production.sh
+++ run_production.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-superset runserver
+superset runserver &>> /home/songyan.ho/shopee-superset/log/superset.log
---
+++ run_worker_production.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+superset worker &>> /home/songyan.ho/shopee-superset/log/superset-worker.log
