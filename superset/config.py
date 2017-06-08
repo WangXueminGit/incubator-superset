30a6a334c7313b55875e3c84113274baf7b2ac50
Xiao Hanyu <xiaohanyu1988@gmail.com>
2017-06-08 19:46:30 +0800

    [#2] Fix CeleryConfig indentation

--- superset/config.py
+++ superset/config.py
@@ -266,11 +266,12 @@ WARNING_MSG = None
 
 # Example:
 class CeleryConfig(object):
-  BROKER_URL = 'redis://localhost:6379/0'
-  CELERY_IMPORTS = ('superset.sql_lab', )
-  CELERY_RESULT_BACKEND = 'redis://localhost:6379/1'
-  CELERY_ANNOTATIONS = {'tasks.add': {'rate_limit': '10/s'}}
-  CELERY_TASK_PROTOCOL = 1
+    BROKER_URL = 'redis://localhost:6379/0'
+    CELERY_IMPORTS = ('superset.sql_lab', )
+    CELERY_RESULT_BACKEND = 'redis://localhost:6379/1'
+    CELERY_ANNOTATIONS = {'tasks.add': {'rate_limit': '10/s'}}
+    CELERY_TASK_PROTOCOL = 1
+
 CELERY_CONFIG = CeleryConfig
 
 # CELERY_CONFIG = None
