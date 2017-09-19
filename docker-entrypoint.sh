c7acf9bdd8f6f5620e214798b12dcdaa98505056
Xiao Hanyu <hanyu.xiao@shopeemobile.com>
2017-09-19 14:35:13 +0800

    Adjust the default number of workers for gunicorn.
    Ref:
    - https://stackoverflow.com/questions/15979428/what-is-the-appropriate-number-of-gunicorn-workers-for-each-amazon-instance-type

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -6,7 +6,7 @@ if [ "$#" -ne 0 ]; then
 elif [ "$LUMOS_ENV" = "test" ]; then
     superset runserver -d
 elif [ "$LUMOS_ENV" = "prod" ]; then
-    superset runserver -a 0.0.0.0
+    superset runserver -a 0.0.0.0 -w $((2 * $(getconf _NPROCESSORS_ONLN) + 1))
 else
     superset --help
 fi
