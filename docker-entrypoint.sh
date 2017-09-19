dded710a71a7bf1e7ac6563a4bbaed11cac722dc
Xiao Hanyu <hanyu.xiao@shopeemobile.com>
2017-09-19 17:17:41 +0800

    Enable four different environment for docker-entrypoint.sh
    - local: local development
    - testing: for shared testing environment
    - staging: pre-production
    - production: as its name

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -3,9 +3,13 @@ set -ex
 
 if [ "$#" -ne 0 ]; then
     exec "$@"
-elif [ "$LUMOS_ENV" = "test" ]; then
+elif [ "$LUMOS_ENV" = "local" ]; then
     superset runserver -d
-elif [ "$LUMOS_ENV" = "prod" ]; then
+elif [ "$LUMOS_ENV" = "testing" ]; then
+    superset runserver -d
+elif [ "$LUMOS_ENV" = "staging" ]; then
+    superset runserver -a 0.0.0.0 -w $((2 * $(getconf _NPROCESSORS_ONLN) + 1))
+elif [ "$LUMOS_ENV" = "production" ]; then
     superset runserver -a 0.0.0.0 -w $((2 * $(getconf _NPROCESSORS_ONLN) + 1))
 else
     superset --help
