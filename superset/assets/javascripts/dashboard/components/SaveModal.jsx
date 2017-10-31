7f08a50db549d06485784ddac7031b5db68fd12e
Xiao Hanyu <hanyu.xiao@shopeemobile.com>
2017-10-31 12:02:45 +0800

    Improve text description for dashboard copy.
    Just let user know that copied dashboard with duplicated slices will
    use the original datasources.

--- superset/assets/javascripts/dashboard/components/SaveModal.jsx
+++ superset/assets/javascripts/dashboard/components/SaveModal.jsx
@@ -146,7 +146,7 @@ class SaveModal extends React.PureComponent {
                 checked={this.state.duplicateSlices}
                 onChange={this.toggleDuplicateSlices.bind(this)}
               />
-              <span className="m-l-5">also copy (duplicate) slices</span>
+              <span className="m-l-5">also copy (duplicate) slices. (Note: copied slices will link to the original data sources.)</span>
             </div>
           </FormGroup>
         }
