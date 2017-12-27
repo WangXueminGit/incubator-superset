de21b690b414bcce1951c2f988ada54867d43a17
Xiao Hanyu <hanyu.xiao@shopeemobile.com>
2017-12-27 17:55:02 +0800

    Enable superset worker for sql lab.

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -4,12 +4,16 @@ set -ex
 if [ "$#" -ne 0 ]; then
     exec "$@"
 elif [ "$LUMOS_ENV" = "local" ]; then
+    superset worker &
     superset runserver -d
 elif [ "$LUMOS_ENV" = "testing" ]; then
+    superset worker &
     superset runserver -d
 elif [ "$LUMOS_ENV" = "staging" ]; then
+    superset worker &
     superset runserver -a 0.0.0.0 -w $((2 * $(getconf _NPROCESSORS_ONLN) + 1))
 elif [ "$LUMOS_ENV" = "production" ]; then
+    superset worker &
     superset runserver -a 0.0.0.0 -w $((2 * $(getconf _NPROCESSORS_ONLN) + 1))
 else
     superset --help
