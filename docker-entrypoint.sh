9009d82a149f64e1b7e95e959e9fc9324601bb14
Wang Xuemin <xuemin.wang@shopee.com>
2018-03-07 18:29:11 +0800

    change entrypoint to make sqllab run in a standalone container

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -10,10 +10,8 @@ elif [ "$LUMOS_ENV" = "testing" ]; then
     superset worker &
     superset runserver -d
 elif [ "$LUMOS_ENV" = "staging" ]; then
-    superset worker &
     superset runserver -a 0.0.0.0 -w $((2 * $(getconf _NPROCESSORS_ONLN) + 1))
 elif [ "$LUMOS_ENV" = "production" ]; then
-    superset worker &
     superset runserver -a 0.0.0.0 -w $((2 * $(getconf _NPROCESSORS_ONLN) + 1))
 else
     superset --help
