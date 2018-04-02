169a34c2ad4afbbc0fcb0114ce6095d4ebb2660c
Xiao Hanyu <xiaohanyu1988@gmail.com>
2018-04-02 19:42:08 +0800

    Enable gevent worker for gunicorn.
    I guess the bottleneck of superset is not in backend, cause right now
    we only serves less than 10 reqs/second, and I guess the bottleneck
    should be in redshift/presto.
    
    But still, let's first try gunicorn with gevent worker to see whether
    it helps, as official documentation recommends.
    
    Ref links:
    - https://superset.incubator.apache.org/installation.html

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -10,9 +10,9 @@ elif [ "$LUMOS_ENV" = "testing" ]; then
     superset worker &
     superset runserver -d
 elif [ "$LUMOS_ENV" = "staging" ]; then
-    superset runserver -a 0.0.0.0 -w $((2 * $(getconf _NPROCESSORS_ONLN) + 1))
+    gunicorn -w $((2 * $(getconf _NPROCESSORS_ONLN) + 1)) -k gevent --timeout 1800 -b 0.0.0.0:8088 --limit-request-line 0 --limit-request-field_size 0 superset:app
 elif [ "$LUMOS_ENV" = "production" ]; then
-    superset runserver -a 0.0.0.0 -w $((2 * $(getconf _NPROCESSORS_ONLN) + 1))
+    gunicorn -w $((2 * $(getconf _NPROCESSORS_ONLN) + 1)) -k gevent --timeout 1800 -b 0.0.0.0:8088 --limit-request-line 0 --limit-request-field_size 0 superset:app
 else
     superset --help
 fi
