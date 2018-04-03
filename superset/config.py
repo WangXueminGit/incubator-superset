5b62b939a9fa466d567c72a85273581570742290
Hao Qin Tan <haoqin.tan@shopee.com>
2018-04-03 19:05:24 +0800

    Added mapbox_api_key to enable mapbox visualizations on Lumos

--- superset/config.py
+++ superset/config.py
@@ -273,7 +273,7 @@ INTERVAL = 1
 BACKUP_COUNT = 30
 
 # Set this API key to enable Mapbox visualizations
-MAPBOX_API_KEY = ""
+MAPBOX_API_KEY = get_env_variable('MAPBOX_API_KEY')
 
 # Maximum number of rows returned in the SQL editor
 SQL_MAX_ROW = 2000000
